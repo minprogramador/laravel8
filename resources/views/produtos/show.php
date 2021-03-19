df83bc896911b633791466317622e670bc93b92e
minprogramador <minprogramador@gmail.com>
2021-03-19 00:36:17 -0300

    template com blade

--- resources/views/produtos/show.php
+++ resources/views/produtos/show.php
@@ -4,6 +4,6 @@
 	<title>Document</title>
 </head>
 <body>
-	<h1>O nome do produto é <?php echo $nome;?></h1>
+	<h1>O nome do produto é <?php echo $nome;?> e seu valor é <?php echo $valor;?></h1>
 </body>
 </html>
